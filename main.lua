4510394f9627f5b96cc77ad21c67b30ce1d25fa9
wtfx0rzz-mark <wtfmark@pm.me>
2025-11-03 16:31:43 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -52,6 +52,7 @@ local paths = {
     Player   = "https://raw.githubusercontent.com/wtfx0rzz-mark/aZ8rY2dLq4NfX1pT9sGv/refs/heads/main/player.lua",
     Auto    = "https://raw.githubusercontent.com/wtfx0rzz-mark/aZ8rY2dLq4NfX1pT9sGv/refs/heads/main/auto.lua",
     Visuals = "https://raw.githubusercontent.com/wtfx0rzz-mark/aZ8rY2dLq4NfX1pT9sGv/refs/heads/main/visuals.lua",
+    TPBring = "https://raw.githubusercontent.com/wtfx0rzz-mark/aZ8rY2dLq4NfX1pT9sGv/refs/heads/main/tpbring.lua",
 }
 
 for name, url in pairs(paths) do
