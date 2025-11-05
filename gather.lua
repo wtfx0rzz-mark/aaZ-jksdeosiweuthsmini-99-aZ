941f8a7416fab406f5ad47e83e36aca46195d037
wtfx0rzz-mark <wtfmark@pm.me>
2025-11-04 19:36:07 -0600

    Update gather.lua

--- gather.lua
+++ gather.lua
@@ -12,7 +12,7 @@ return function(C, R, UI)
         "Tyre","Bolt","Broken Fan","Broken Microwave","Sheet Metal","Old Radio","Washing Machine","Old Car Engine",
         "UFO Junk","UFO Component"
     }
-    local fuelItems    = { "Log","Chair","Coal","Fuel Canister","Oil Barrel" }
+    local fuelItems    = { "Log","Coal","Fuel Canister","Oil Barrel","Chair" }
     local foodItems    = {
         "Cake","Cooked Steak","Cooked Morsel","Steak","Morsel","Berry","Carrot",
         "Chilli","Stew","Ribs","Pumpkin","Hearty Stew","Cooked Ribs","Corn","BBQ ribs","Apple","Mackerel"
