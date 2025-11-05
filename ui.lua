aeb929007807a39853d7cf26c98310f97b1905cf
wtfx0rzz-mark <wtfmark@pm.me>
2025-11-04 19:29:59 -0600

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -40,6 +40,7 @@ local Tabs = {
     Auto   = Window:Tab({ Title = "Auto",   Icon = "cpu",   Desc = "Automation" }),
     Visuals   = Window:Tab({ Title = "Visuals",   Icon = "eye",   Desc = "Visuals" }),
     TPBring   = Window:Tab({ Title = "TPBring",   Icon = "backpack",   Desc = "TP Bring" }),
+    Debug   = Window:Tab({ Title = "Debug",   Icon = "ant",   Desc = "Debug" })
 }
 
 return {
